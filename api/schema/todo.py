91e850ab53a185113228963ebc976147dab02604
Macshellah Zisengwe <mpzisengwe@gmail.com>
2024-12-01 03:16:42 +0000

    addin schema

--- api/schema/todo.py
+++ api/schema/todo.py
@@ -2,3 +2,12 @@ from pydantic import BaseModel, Field
 from typing import Optional
 from tortoise.contrib.pydantic import pyydantic_model_creator
 
+GetLocs = pyydantic_model_creator(None, name="Todo")
+
+class PostLocs(BaseModel):
+    task: str = Field(..., max_length=100)
+    done:bool
+
+class PutLocs(BaseModel):
+    task: Optional[str] = Field(None, max_length=100)
+    done: Optional[bool]
